fc52c9166df49a11616a51e4a571c6cd7e63ca6f
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-06-03 18:47:07 -0400

    update version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_desc = open(path('README.md')).read()
 
 setup(
     name='esFrontLine',
-    version="1.1.14230",
+    version="2.11.18154",
     description='Limit restful requests to backend ElasticSearch cluster:  Queries only.',
     long_description=long_desc,
     author='Kyle Lahnakoski',
@@ -18,11 +18,7 @@ setup(
     url='https://github.com/klahnakoski/esFrontLine',
     license='MPL 2.0',
     packages=find_packages(),
-    install_requires=[
-        'Flask==0.10.1',
-        'requests==2.3.0',
-        'mohawk==0.3.4',
-    ],
+    install_requires=["Flask==0.10.1","elasticsearch==6.2.0","mo-dots>=2.7.18148","mo-future>=2.3.18147","mo-logs","mohawk==0.3.4","requests==2.18.4","responses==0.9.0"],
     include_package_data=True,
     zip_safe=False,
     entry_points={
